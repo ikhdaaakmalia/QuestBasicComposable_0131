fdae7dc76751d131a38ca413f35627c649cbf889
ikhdaakmalia <punyaamelaja@gmail.com>
2024-10-17 10:58:49 +0700

    membuat style untuk menjadikan font tebal/bold

--- app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
@@ -53,7 +53,8 @@ fun BasicLayout(modifier: Modifier = Modifier) {
         Image(painter = painterResource(id = R.drawable.umy), contentDescription = null,
             modifier = Modifier.padding(top = 30.dp). size(150.dp))
         Text(text = "Nama :",
-            style = TextStyle(fontSize = 20.sp)
+            style = TextStyle(fontSize = 20.sp,
+                fontWeight = FontWeight.Bold))
     }
 }
 
