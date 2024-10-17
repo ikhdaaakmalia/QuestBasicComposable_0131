942a6c26439ae8e21cbc4d9f43c73c0ad5501956
ikhdaakmalia <punyaamelaja@gmail.com>
2024-10-17 08:44:56 +0700

    membuat column horizontal ditengah

--- app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_0131.ui.theme.QuestBasicComposable_0131Theme
@@ -32,10 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){}
 }
 
 @Preview(showBackground = true)
