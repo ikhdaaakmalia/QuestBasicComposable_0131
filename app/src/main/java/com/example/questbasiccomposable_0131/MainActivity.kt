8ee8d1469a39ba4cd0bfc109e0d26303ed47fcd5
ikhdaakmalia <punyaamelaja@gmail.com>
2024-10-17 11:00:44 +0700

    memberikan style dengan mengubah menjadikan ukuran font menjadi 15.sp

--- app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_0131/MainActivity.kt
@@ -55,7 +55,8 @@ fun BasicLayout(modifier: Modifier = Modifier) {
         Text(text = "Nama :",
             style = TextStyle(fontSize = 20.sp,
                 fontWeight = FontWeight.Bold))
-        Text(text = "IKHDA AKMALIA PUTRI")
+        Text(text = "IKHDA AKMALIA PUTRI",
+            style = TextStyle (fontSize = 15.sp)
     }
 }
 
